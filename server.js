fd18b5ca0adcad0de7ad16d29087f09a6cca5ff4
Ammar-Abid92 <72232879+Ammar-Abid92@users.noreply.github.com>
2021-06-08 00:56:12 +0500

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get("/", function (req , res){
   });
 
 
-
-app.listen(3000, function() {
+  const port = process.env.PORT || 5000;
+app.listen(port, function() {
     console.log("Server started on port 3000");
   });
