9993377bb296277846947bcf5ef85f5fb51e8f59
Ammar Abid <ammarabid890@gmail.com>
2021-06-11 01:41:13 +0500

    Done with Firebase

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,79 @@
+
+
+const inputNameField = document.getElementById("name_");
+const inputEmailField = document.getElementById("email_");
+const inputsmsField = document.getElementById("sms_");
+const buttonSave = document.querySelector("#saveButton");
+
+const database = firebase.database();
+buttonSave.addEventListener('click', (e)=> {
+ e.preventDefault();
+ window.alert("Thanks For the sms");
+ 
+  database.ref('/users/'+ Math.floor((Math.random() * 100) + 1)).set({
+    nameStatus: inputNameField.value,
+    emailStatus: inputEmailField.value,
+    smsStatus: inputsmsField.value,
+
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDG1CNRjRjuvevWuVUD65dSDCITCNO2hKw",
+//   authDomain: "softsolutions-012.firebaseapp.com",
+//   databaseURL: "https://softsolutions-012-default-rtdb.firebaseio.com",
+//   projectId: "softsolutions-012",
+//   storageBucket: "softsolutions-012.appspot.com",
+//   messagingSenderId: "345862077731",
+//   appId: "1:345862077731:web:0ac000d678aea6e2fa6096",
+//   measurementId: "G-2WW3NFVCGV"
+// };
+// firebase.initializeApp(config);
+// var firestore = firebase.firestore();
+
+// const docRef = firestore.doc("samples/userData");
+// const inputNameField = document.querySelector("#name_");
+// const inputEmailField = document.querySelector("#email_");
+// const inputsmsField = document.querySelector("#sms_");
+// const saveButton = document.querySelector("#saveButton");
+
+
+// saveButton.addEventListener("click", function(){
+//   const nameToSave = inputNameField.value;
+//   const emailToSave = inputEmailField.value;
+//   const smsToSave = inputsmsField.value;
+//   console.log("I am going to save" +nameToSave +","+ emailToSave + "," + smsToSave);
+//   docRef.set({
+//     nameStatus: nameToSave,
+//     emailStatus: emailToSave,
+//     smsStatus: smsToSave,
+
+//   }).then(function(){
+//     console.log("status saved!");
+//   }).catch(function(error){
+//     console.log("Got an error", error);
+//   });
+// });
+
 const MoonPath =
   "M14 28C14 43.464 27.5 55 27.5 56C12.3122 56 0 43.464 0 28C0 12.536 12.3122 0 27.5 0C26.9908 0.00470297 14 12.536 14 28Z";
 
